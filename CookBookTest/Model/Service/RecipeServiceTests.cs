75589fa8d44e0f5e5f9a0a63690af4bd7f8c043c
kkris <k.krisztoferbela@gmail.com>
2022-11-18 18:49:42 +0100

    Teszt javítása

---
+++ CookBookTest/Model/Service/RecipeServiceTests.cs
@@ -0,0 +1,29 @@
+﻿using CookBookApp.Models;
+using CookBookApp.Models.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CookBookTest.Model.Service
+{
+    public class RecipeServiceTests
+    {
+        [Fact]
+        public void getRecipesJoinedAsync_GetRecipes()
+        {
+            //Arrange
+            List<Recipe> expectedRecipes = new List<Recipe>();
+
+            //Act
+            List<Recipe> actualRecipes;
+            Task.Run(async () =>
+            {
+                actualRecipes = await new RecipeService().getRecipesJoinedAsync();
+            });
+            Console.WriteLine("");
+        }
+    }
+}
