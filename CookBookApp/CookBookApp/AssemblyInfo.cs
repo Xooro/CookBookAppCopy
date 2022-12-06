63fb130e32015e073b6eb62102da11dd8aa01b65
kkris <k.krisztoferbela@gmail.com>
2022-12-06 22:50:21 +0100

    bugfix

--- CookBookApp/CookBookApp/AssemblyInfo.cs
+++ CookBookApp/CookBookApp/AssemblyInfo.cs
@@ -1,8 +1,4 @@
-using Android.App;
 using System.Resources;
-using Xamarin.Forms.PlatformConfiguration;
 using Xamarin.Forms.Xaml;
 [assembly: NeutralResourcesLanguage("en-US")]
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
-
-[assembly: UsesPermission(Android.Manifest.Permission.ReadExternalStorage)]
