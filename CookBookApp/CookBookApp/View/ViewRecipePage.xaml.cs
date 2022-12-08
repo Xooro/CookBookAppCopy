a2832e3bc65f6476b9d3ae7431ff043994de6a6d
akile <akile@TEKILA>
2022-12-08 19:53:31 +0100

    ViewRecipe UI kezdet

---
+++ CookBookApp/CookBookApp/View/ViewRecipePage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using CookBookApp.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CookBookApp.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ViewRecipePage : ContentPage
+    {
+        public ViewRecipePage()
+        {
+            InitializeComponent();
+            BindingContext = new ViewRecipeViewModel();
+        }
+    }
+}
---
+++ CookBookApp/CookBookApp/ViewModel/ViewRecipeViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace CookBookApp.ViewModel
+{
+    public class ViewRecipeViewModel
+    {
+        public ObservableCollection<string> testList { get; set; } 
+
+        public ViewRecipeViewModel()
+        {
+            testList = new ObservableCollection<string>() { "test1", "test2", "test3", "test4" };
+        }
+    }
+}
