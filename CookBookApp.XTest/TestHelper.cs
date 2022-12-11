7ff80f71ffd1c13e05118933649608bb0c59d797
Krisztofer Kiss <k.krisztoferbela@gmail.com>
2022-12-11 15:09:35 +0100

    XUnit hozzáadása ismét

--- CookBookApp.XTest/TestHelper.cs
+++ CookBookApp.XTest/TestHelper.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CookBookApp.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,16 +9,16 @@ namespace CookBookApp.XTest
 {
     public class TestHelper
     {
-        public static RecipeContext getFilledRecipeConext()
-        {
-            var conn = new SqliteConnection("DataSource=:memory:");
-            conn.Open(); // open connection to use
-            var options = new DbContextOptionsBuilder<ShoppingContext>()
-               .UseSqlite(conn)
-               .Options;
-            RecipeContext context = new RecipeContext(options);
+        //public static RecipeContext getFilledRecipeConext()
+        //{
+        //    var conn = new SqliteConnection("DataSource=:memory:");
+        //    conn.Open(); // open connection to use
+        //    var options = new DbContextOptionsBuilder<ShoppingContext>()
+        //       .UseSqlite(conn)
+        //       .Options;
+        //    RecipeContext context = new RecipeContext(options);
 
-            return context;
-        }
+        //    return context;
+        //}
     }
 }
